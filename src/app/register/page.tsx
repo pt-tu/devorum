4a9e034f32530101c828403f812d39cf1cca0a5b
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-10-24 16:46:53 +0700

    finish validation of auth form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,41 +6,42 @@ import NextImage from 'next/image'
 import ThirdPartiesAuth from '@/components/auth/ThirdPartiesAuth'
 import { Dispatch, SetStateAction, useState } from 'react'
 import { PasswordInput } from '@/components/auth'
+import { useFormik } from 'formik'
+import validationSchema from './validation'
+import RegisterForm from '@/components/auth/RegisterForm'
 
 const Register = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
   const [pwdVisible, setPwdVisible] = useState(false)
   const [confPwdVisible, setConfPwdVisible] = useState(false)
-
-  const toggleVisibility = (setFn: Dispatch<SetStateAction<boolean>>) => () => {
-    setFn((prev) => !prev)
-  }
-
-  const handleSubmit = () => {}
 
   return (
     <div className='text-center bg-white rounded-3xl px-16 py-14 text-base shadow-2xl'>
       <div className='flex justify-center'>
-        <Image as={NextImage} width={150} height={75} src='/static/logo_dark.svg' alt='devorum_logo' />
+        <Image as={NextImage} width={150} height={75} src='/logo_dark.svg' alt='devorum_logo' />
       </div>
 
       <h1 className='font-medium text-3xl mt-6'>Join the devorum community</h1>
       <p className='mt-4'>You give us some texts, we give you the whole community. Best deal ever!</p>
 
-      <form className='mt-10' onSubmit={handleSubmit}>
-        <Input label='Email' required name='email' size='lg' />
-        <Input label='Username' required name='username' size='lg' className='mt-4' />
-        <PasswordInput visible={pwdVisible} toggle={toggleVisibility(setPwdVisible)} label='Passcode' name='password' />
-        <PasswordInput
-          visible={confPwdVisible}
-          toggle={toggleVisibility(setConfPwdVisible)}
-          label='Passcode confirm'
-          name='confirm-password'
-        />
-
-        <Button size='lg' fullWidth className='mt-8 bg-black text-white'>
-          Register
-        </Button>
-      </form>
+      <RegisterForm
+        formik={formik}
+        pwdVisible={pwdVisible}
+        setPwdVisible={setPwdVisible}
+        confPwdVisible={confPwdVisible}
+        setConfPwdVisible={setConfPwdVisible}
+      />
 
       <div className='flex justify-center items-center gap-4 mt-6 text-sm'>
         <p className='w-6 border-t'></p>
---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,73 @@
+import { Button, Input } from '@nextui-org/react'
+import React, { Dispatch, SetStateAction } from 'react'
+import PasswordInput from './PasswordInput'
+
+type Props = {
+  formik: any
+  pwdVisible: boolean
+  setPwdVisible: Dispatch<SetStateAction<boolean>>
+  confPwdVisible: boolean
+  setConfPwdVisible: Dispatch<SetStateAction<boolean>>
+}
+
+const RegisterForm = ({ formik, pwdVisible, setPwdVisible, confPwdVisible, setConfPwdVisible }: Props) => {
+  const toggleVisibility = (setFn: Dispatch<SetStateAction<boolean>>) => () => {
+    setFn((prev) => !prev)
+  }
+
+  return (
+    <form className='mt-10' onSubmit={formik.handleSubmit}>
+      <Input
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        isInvalid={formik.touched.email && Boolean(formik.errors.email)}
+        errorMessage={formik.touched.email && formik.errors.email}
+        label='Email'
+        required
+        name='email'
+        size='lg'
+      />
+      <Input
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        isInvalid={formik.touched.username && Boolean(formik.errors.username)}
+        errorMessage={formik.touched.username && formik.errors.username}
+        label='Username'
+        required
+        name='username'
+        size='lg'
+        className='mt-4'
+      />
+      <PasswordInput
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        isInvalid={formik.touched.password && Boolean(formik.errors.password)}
+        errorMessage={formik.touched.password && formik.errors.password}
+        visible={pwdVisible}
+        toggle={toggleVisibility(setPwdVisible)}
+        label='Passcode'
+        name='password'
+      />
+      <PasswordInput
+        value={formik.values.confirmPassword}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        isInvalid={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+        errorMessage={formik.touched.confirmPassword && formik.errors.confirmPassword}
+        visible={confPwdVisible}
+        toggle={toggleVisibility(setConfPwdVisible)}
+        label='Passcode Confirm'
+        name='confirmPassword'
+      />
+
+      <Button type='submit' size='lg' fullWidth className='mt-8 bg-black text-white'>
+        Register
+      </Button>
+    </form>
+  )
+}
+
+export default RegisterForm

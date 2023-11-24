e7dd74b06ccfcd848a88c783906c9272411c963f
Phan Thanh Tú <pttu2902@gmail.com>
2023-11-24 09:59:46 +0700

    add something

--- src/components/common/PostItem.tsx
+++ src/components/common/PostItem.tsx
@@ -28,8 +28,12 @@ const items: MenuProps['items'] = [
 function PostItem(props: PostProps) {
   const router = useRouter()
   const { increaseVote, setIsEditing, updatePost } = usePostStore()
-  const [title, setTitle] = useState(props.title)
-  const [content, setContent] = useState(props.content)
+  const [title, setTitle] = useState<string>(props.title)
+  const [content, setContent] = useState<string>(props.content)
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
 
   useEffect(() => {
     setTitle(props.title)
@@ -61,68 +65,73 @@ function PostItem(props: PostProps) {
   }
 
   return (
-    <div className="mb-6 flex flex-col gap-6 rounded-2xl bg-dark-2 px-8 py-6">
-      {/* Header */}
-      <div className="flex max-h-min flex-row">
-        <img className="h-9 w-9 rounded-full" alt="devorum_avt" src={props.user.url} />
-        <div className="ml-4 h-full flex-1">
-          <p className="text-sm font-normal text-gray-bg">{props.user.name}</p>
-          <p className="text-[10px] font-light text-gray-400">{moment(props.time).fromNow()}</p>
+    mounted && (
+      <div className="mb-6 flex flex-col gap-6 rounded-2xl bg-dark-2 px-8 py-6">
+        {/* Header */}
+        <div className="flex max-h-min flex-row">
+          <img className="h-9 w-9 rounded-full" alt="devorum_avt" src={props.user.url} />
+          <div className="ml-4 h-full flex-1">
+            <p className="text-sm font-normal text-gray-bg">{props.user.name}</p>
+            <p className="text-[10px] font-light text-gray-400">{'moment(props.time).fromNow()'}</p>
+          </div>
+          <Dropdown menu={{ items, onClick }} placement="bottomRight">
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                <MoreOutlined className="text-2xl text-gray-3" />
+              </Space>
+            </a>
+          </Dropdown>
         </div>
-        <Dropdown menu={{ items, onClick }} placement="bottomRight">
-          <a onClick={(e) => e.preventDefault()}>
-            <Space>
-              <MoreOutlined className="text-2xl text-gray-3" />
-            </Space>
-          </a>
-        </Dropdown>
+        {/* Body */}
+        <div onClick={handlePostClick} className={classNames(!props.isEditing && 'cursor-pointer')}>
+          {props.isEditing ? (
+            <>
+              <Input value={title} className="mb-5" onChange={(e) => setTitle(e.target.value)} />
+              <MDEditor value={content} onChange={(e) => setContent(e || '')} />
+              <div className="mt-5 flex flex-row justify-end gap-4">
+                <Button size="md" radius="sm" onClick={() => setIsEditing(props.postId)}>
+                  Cancel
+                </Button>
+                <Button size="md" radius="sm" color="primary" onClick={handleSaveClick}>
+                  Save
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <p className="mb-[10px] text-base font-medium text-gray-bg">{props.title}</p>
+              <Markdown
+                source={props.content}
+                className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap text-sm font-light text-gray-3"
+              />
+            </>
+          )}
+        </div>
+        {/* Footer */}
+        <div className="flex flex-row items-center gap-4">
+          {props?.tags && <Tags tags={props.tags} />}
+          <div className="flex-1" />
+          <div className="flex flex-row items-center gap-1 text-sm text-gray-3">
+            <EyeOutlined />
+            <p>{props.react.views}</p>
+          </div>
+          <div
+            className="flex cursor-pointer flex-row items-center gap-1 text-sm text-gray-3"
+            onClick={handlePostClick}
+          >
+            <MessageOutlined />
+            <p>{props.react.comments}</p>
+          </div>
+          <div
+            className="flex cursor-pointer flex-row items-center gap-1 text-sm text-gray-3"
+            onClick={() => increaseVote(props.postId)}
+          >
+            <ArrowUpOutlined />
+            <p>{props.react.votes}</p>
+          </div>
+        </div>
       </div>
-      {/* Body */}
-      <div onClick={handlePostClick} className={classNames(!props.isEditing && 'cursor-pointer')}>
-        {props.isEditing ? (
-          <>
-            <Input value={title} className="mb-5" onChange={(e) => setTitle(e.target.value)} />
-            <MDEditor value={content} onChange={(e) => setContent(e || '')} />
-            <div className="mt-5 flex flex-row justify-end gap-4">
-              <Button size="md" radius="sm" onClick={() => setIsEditing(props.postId)}>
-                Cancel
-              </Button>
-              <Button size="md" radius="sm" color="primary" onClick={handleSaveClick}>
-                Save
-              </Button>
-            </div>
-          </>
-        ) : (
-          <>
-            <p className="mb-[10px] text-base font-medium text-gray-bg">{props.title}</p>
-            <Markdown
-              source={props.content}
-              className="w-full overflow-hidden overflow-ellipsis whitespace-nowrap text-sm font-light text-gray-3"
-            />
-          </>
-        )}
-      </div>
-      {/* Footer */}
-      <div className="flex flex-row items-center gap-4">
-        {props?.tags && <Tags tags={props.tags} />}
-        <div className="flex-1" />
-        <div className="flex flex-row items-center gap-1 text-sm text-gray-3">
-          <EyeOutlined />
-          <p>{props.react.views}</p>
-        </div>
-        <div className="flex cursor-pointer flex-row items-center gap-1 text-sm text-gray-3" onClick={handlePostClick}>
-          <MessageOutlined />
-          <p>{props.react.comments}</p>
-        </div>
-        <div
-          className="flex cursor-pointer flex-row items-center gap-1 text-sm text-gray-3"
-          onClick={() => increaseVote(props.postId)}
-        >
-          <ArrowUpOutlined />
-          <p>{props.react.votes}</p>
-        </div>
-      </div>
-    </div>
+    )
   )
 }
 

fa61249a5f1019a719a8e736e1f83988c162ff03
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-11 14:06:17 +0700

    add reply and likes

--- src/app/(main)/m/[username]/Message.tsx
+++ src/app/(main)/m/[username]/Message.tsx
@@ -1,13 +1,24 @@
 import { useThemeStore } from '@/store/useThemeStore'
-import { Avatar } from '@nextui-org/react'
+import { Avatar, Button } from '@nextui-org/react'
 import classNames from 'classnames'
-import React from 'react'
+import React, { useState } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { IoIosHeart } from 'react-icons/io'
 
 type Props = {
   isSelf?: boolean
   language?: string
+  replyTo?: {
+    content: string
+    username: string
+  }
+  likes?: number
+  // isReplyingTo?: {
+  //   content: string
+  //   username: string
+  // }
+  setIsReplyingTo: (value: any) => void
 }
 const codeString = `class HelloMessage extends React.Component {
   handlePress = () => {
@@ -29,26 +40,52 @@ ReactDOM.render(
 );
 `
 
-const Message = ({ isSelf, language }: Props) => {
+const Message = ({ isSelf, language, replyTo, setIsReplyingTo, likes = 0 }: Props) => {
   const theme = useThemeStore((state) => state.theme)
 
   return (
-    <div className={classNames('flex w-full gap-4', isSelf && 'flex-row-reverse')}>
-      <Avatar size="lg" />
+    <div className={classNames(' flex w-full gap-4', isSelf && 'flex-row-reverse')}>
+      {!isSelf && <Avatar size="lg" />}
       <div className="max-w-[80%]">
-        {language ? (
-          <SyntaxHighlighter
-            className={classNames('font-mono font-light', theme === 'light' && 'invert')}
-            language="javascript"
-            style={atomDark}
-            showLineNumbers
-            wrapLongLines
+        <div
+          onDoubleClick={() => setIsReplyingTo({ content: 'Hello baby', username: 'tuan-hda' })}
+          className={classNames(
+            'group relative rounded-xl p-4 font-light',
+            !isSelf ? 'bg-default-100' : 'bg-primary-300',
+          )}
+        >
+          {replyTo && (
+            <div className="mb-2 rounded-lg bg-default-300/50 p-2 text-sm">
+              <p className="font-normal">tuan-hda</p>
+              <p className="font-light">Hello 500 anh em</p>
+            </div>
+          )}
+          {language ? (
+            <SyntaxHighlighter
+              className={classNames('font-mono font-light', theme === 'light' && 'invert')}
+              language="javascript"
+              style={atomDark}
+              showLineNumbers
+              wrapLongLines
+            >
+              {codeString}
+            </SyntaxHighlighter>
+          ) : (
+            'Hello world'
+          )}
+          <div
+            className={classNames('absolute -bottom-2 group-hover:opacity-100', likes == 0 && 'opacity-0')}
+            style={{
+              left: isSelf ? `-${String(likes).length * 9 + 8}px` : 'auto',
+              right: !isSelf ? `-${String(likes).length * 9 + 8}px` : 'auto',
+            }}
           >
-            {codeString}
-          </SyntaxHighlighter>
-        ) : (
-          <div className={classNames('rounded-xl p-4', !isSelf ? 'bg-default-100' : 'bg-primary-300')}>Hello world</div>
-        )}
+            <Button size="sm" isIconOnly radius="full" className="flex w-fit gap-1 px-2 text-xs font-light">
+              <IoIosHeart className="text-red-500" />
+              {likes > 1 && likes}
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/app/(main)/m/[username]/page.tsx
+++ src/app/(main)/m/[username]/page.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { Avatar, Button, Input } from '@nextui-org/react'
-import React from 'react'
-import { IoSend } from 'react-icons/io5'
+import { Avatar } from '@nextui-org/react'
+import React, { useState } from 'react'
 import Message from './Message'
-import { MdOutlinePermMedia } from 'react-icons/md'
 import MessageBox from './MessageBox'
 
 const MessageRoom = () => {
+  const [isReplyingTo, setIsReplyingTo] = useState<{ content: string; username: string }>()
+
   return (
     <div className="relative col-span-9 h-full bg-dark-5">
       <div className="absolute z-10 flex h-[72px] w-full items-center gap-4 border-l border-l-dark-2 bg-dark-2/50 px-6 shadow-md backdrop-blur-md">
@@ -18,25 +18,28 @@ const MessageRoom = () => {
       </div>
       <div className="small-scrollbar h-[calc(100vh-168px)] w-full overflow-y-auto">
         <div className="m-auto max-w-2xl space-y-4 px-1 pb-2 pt-20">
-          <Message />
-          <Message />
-          <Message />
-          <Message /> <Message />
-          <Message />
-          <Message />
-          <Message /> <Message />
-          <Message />
-          <Message />
-          <Message /> <Message />
-          <Message />
-          <Message />
-          <Message /> <Message />
-          <Message />
-          <Message isSelf />
-          <Message language="javascript" />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message likes={2234} setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} replyTo={{ content: '', username: 'tuan-hda' }} />
+          <Message setIsReplyingTo={setIsReplyingTo} /> <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} />
+          <Message setIsReplyingTo={setIsReplyingTo} replyTo={{ content: '', username: 'tuan-hda' }} isSelf />
+          <Message setIsReplyingTo={setIsReplyingTo} language="javascript" />
         </div>
       </div>
-      <MessageBox />
+      <MessageBox isReplyingTo={isReplyingTo} setIsReplyingTo={setIsReplyingTo} />
     </div>
   )
 }

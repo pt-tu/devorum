2e6094340e5978e680b1f12ac4ae8d8155f1b8b7
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-11 14:06:17 +0700

    add reply and likes

--- src/app/(main)/m/[username]/MessageBox.tsx
+++ src/app/(main)/m/[username]/MessageBox.tsx
@@ -1,9 +1,18 @@
 import { Button, Input } from '@nextui-org/react'
 import React, { useRef } from 'react'
-import { IoSend } from 'react-icons/io5'
+import { IoClose, IoSend } from 'react-icons/io5'
 import { MdOutlinePermMedia } from 'react-icons/md'
+import { FaReply } from 'react-icons/fa'
 
-const MessageBox = () => {
+type Props = {
+  isReplyingTo?: {
+    content: string
+    username: string
+  }
+  setIsReplyingTo: (value: any) => void
+}
+
+const MessageBox = ({ isReplyingTo, setIsReplyingTo }: Props) => {
   const ref = useRef<HTMLInputElement | null>(null)
 
   const handleClick = () => {
@@ -11,17 +20,39 @@ const MessageBox = () => {
   }
 
   return (
-    <div className="m-auto flex h-20 max-w-2xl gap-6 pb-2 pt-2">
+    <div className=" m-auto flex h-20 max-w-2xl flex-shrink-0 gap-6 pb-2 pt-2">
       <input type="file" ref={ref} className="hidden" accept=".jpg,.jpeg,.png" />
-      <Input
-        startContent={
-          <Button onClick={handleClick} isIconOnly variant="light" radius="full">
-            <MdOutlinePermMedia className="text-2xl" />
-          </Button>
-        }
-        size="lg"
-        className="h-full w-full rounded-xl"
-      ></Input>
+      <div className="relative flex-1">
+        {isReplyingTo && (
+          <div className="absolute -top-16 left-0 right-0 z-10 flex items-center gap-4 rounded-xl bg-default-100 px-4 py-2 text-sm">
+            <FaReply className="text-xl" />
+
+            <div className="">
+              <p className="font-normal">tuan-hda</p>
+              <p className="font-light">Hello world</p>
+            </div>
+
+            <Button
+              onClick={() => setIsReplyingTo(undefined)}
+              isIconOnly
+              variant="light"
+              className="ml-auto"
+              radius="full"
+            >
+              <IoClose className="text-2xl" />
+            </Button>
+          </div>
+        )}
+        <Input
+          startContent={
+            <Button onClick={handleClick} isIconOnly variant="light" radius="full">
+              <MdOutlinePermMedia className="text-2xl" />
+            </Button>
+          }
+          size="lg"
+          className="h-full w-full rounded-xl"
+        ></Input>
+      </div>
       <div className="aspect-square h-full">
         <Button isIconOnly radius="full" size="lg" color="primary" className="h-full w-full">
           <IoSend className="text-xl" />

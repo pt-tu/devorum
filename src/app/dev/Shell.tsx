186d2bee59915679651d4d90e709efd775221b62
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-15 15:29:20 +0700

    fix shell not working with container webssh on backend

--- src/app/dev/Shell.tsx
+++ src/app/dev/Shell.tsx
@@ -1,4 +1,5 @@
 'use client'
+import configs from '@/configs/configs'
 import React, { CSSProperties, useEffect, useRef } from 'react'
 
 type Props = {
@@ -6,9 +7,11 @@ type Props = {
 }
 
 const Shell = ({ fontSize = '13px' }: Props) => {
+  console.log(process.env.NEXT_SHELL_HOSTNAME)
+  console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
   return (
     <div className="small-scrollbar h-full overflow-auto rounded-xl bg-black p-1">
-      <iframe src="http://localhost:2222/ssh/host/127.0.0.1" className="h-full w-full"></iframe>
+      <iframe src={`http://localhost:2222/ssh/host/${configs.SHELL_HOSTNAME}`} className="h-full w-full"></iframe>
     </div>
   )
 }
--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -1,5 +1,6 @@
 const configs = {
   BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost/',
+  SHELL_HOSTNAME: process.env.NEXT_PUBLIC_SHELL_HOSTNAME || '127.0.0.1',
 }
 
 export default configs
